aa719f077b5ea3c164dc27bb6251faef5846806f
Trxpl1x <144318653+HunkUmbrella@users.noreply.github.com>
2023-09-12 11:02:44 +0400

    2

--- WinFormsApp1/WinFormsApp1/Form1.cs
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -12,5 +12,17 @@ namespace WinFormsApp1
             Form2 newform = new Form2();
             newform.Show();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 newform = new Form3();
+            newform.Show();
+        }
+
+      //  private void button3_Click(object sender, EventArgs e)
+       // {
+       //     Form4 newform = new Form4();
+       //     newform.Show();
+       // }
     }
 }
